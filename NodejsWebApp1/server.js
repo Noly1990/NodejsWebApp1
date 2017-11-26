312c5dffc228fc6c03095e30ac9f67d5875deb6f
YanJun <715285974@qq.com>
2017-11-26 15:07:04 +0800

    'add sth'

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,8 +1,34 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 1337;
+var url = require('url');
+var events = require('events');
+var Koa = require('koa');
+var app = new Koa();
+var server = http.Server(app.callback());
+var io = require('socket.io')(server);
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var port = process.env.PORT || 8888;
+
+app.use(async (ctx) => {
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream(__dirname+'/test.html');
+})
+
+
+server.listen(port, function () {
+    console.log('server is listening on port :'+port)
+});
+
+
+var str = '这是一个字符串';
+var buf = new Buffer(str, 'utf-8');
+console.log(buf.toString())
+console.log('我是严骏')
+
+var emitter = new events.EventEmitter();
+
+emitter.on('myEvent', function (data) {
+    console.log(data)
+});
+
